83defe3f64d486294dd0cd0000691eb1ac4b97c2
Alex Miller <alex.k.miller@gmail.com>
2021-01-19 16:45:03 -0500

    XLSX and slight tweaks

--- crisis_tracking.R
+++ crisis_tracking.R
@@ -1,4 +1,4 @@
-list.of.packages <- c("data.table","anytime","reshape2")
+list.of.packages <- c("data.table","anytime","reshape2","openxlsx")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 lapply(list.of.packages, require, character.only=T)
@@ -6,12 +6,8 @@ lapply(list.of.packages, require, character.only=T)
 setwd("~/git/fts-crisis-tracking")
 
 fts = fread("fts.csv")
-org_ids = fread("org_ids.csv")
-org_ids$abbreviation = NULL
-setnames(org_ids,"id","source_Organization_id")
-setnames(org_ids,"name","source_Organization_name")
 
-overlap_org_df = data.frame(source_Organization_name=c(
+overlap_orgs = c(
   "African Development Bank",
   "Asian Development Bank",
   "World Bank",
@@ -19,9 +15,7 @@ overlap_org_df = data.frame(source_Organization_name=c(
   "Caribbean Development Bank",
   "Inter-American Development Bank",
   "International Bank for Reconstruction & Development (World Bank)"
-))
-overlap_org_df = merge(overlap_org_df, org_ids, by="source_Organization_name", all.x=T)
-overlap_orgs = as.character(overlap_org_df$source_Organization_id)
+)
 
 wb_groupings = fread("wb_groupings.csv")
 setnames(wb_groupings,"iso3c","Country Code")
@@ -52,18 +46,36 @@ for(i in 1:nrow(loop_param_df)){
   ctry_dat = subset(ctry_dat, analysis_date>=start_date & analysis_date<=end_date)
   
   ctry_dat$emergency_boundary = NA
-  ctry_dat$emergency_boundary[which(
-    ctry_dat$source_Emergency_name=="" &
-      grepl(emergency_name, ctry_dat$destination_Emergency_name, fixed=T)
-  )] = "Incoming"
-  ctry_dat$emergency_boundary[which(
-    grepl(emergency_name, ctry_dat$source_Emergency_name, fixed=T) &
-      grepl(emergency_name, ctry_dat$destination_Emergency_name, fixed=T)
-  )] = "Internal"
-  ctry_dat$emergency_boundary[which(
-    grepl(emergency_name, ctry_dat$source_Emergency_name, fixed=T) &
-      !grepl(emergency_name, ctry_dat$destination_Emergency_name, fixed=T)
-  )] = "Outgoing"
+  if(emergency_name!=""){
+    ctry_dat$emergency_boundary[which(
+      ctry_dat$source_Emergency_name=="" &
+        grepl(emergency_name, ctry_dat$destination_Emergency_name, fixed=T)
+    )] = "Incoming"
+    ctry_dat$emergency_boundary[which(
+      grepl(emergency_name, ctry_dat$source_Emergency_name, fixed=T) &
+        grepl(emergency_name, ctry_dat$destination_Emergency_name, fixed=T)
+    )] = "Internal"
+    ctry_dat$emergency_boundary[which(
+      grepl(emergency_name, ctry_dat$source_Emergency_name, fixed=T) &
+        !grepl(emergency_name, ctry_dat$destination_Emergency_name, fixed=T)
+    )] = "Outgoing"
+  }
+  
+  ctry_dat$plan_boundary = NA
+  if(appeal_name!=""){
+    ctry_dat$plan_boundary[which(
+      ctry_dat$source_Plan_name=="" &
+        grepl(appeal_name, ctry_dat$destination_Plan_name, fixed=T)
+    )] = "Incoming"
+    ctry_dat$plan_boundary[which(
+      grepl(appeal_name, ctry_dat$source_Plan_name, fixed=T) &
+        grepl(appeal_name, ctry_dat$destination_Plan_name, fixed=T)
+    )] = "Internal"
+    ctry_dat$plan_boundary[which(
+      grepl(appeal_name, ctry_dat$source_Plan_name, fixed=T) &
+        !grepl(appeal_name, ctry_dat$destination_Plan_name, fixed=T)
+    )] = "Outgoing"
+  }
   
   ctry_dat$row.name = c(1:nrow(ctry_dat))
   
@@ -81,14 +93,18 @@ for(i in 1:nrow(loop_param_df)){
   ctry_dat_analysis = rbind(ctry_dat_analysis, ctry_dat_remainder)
   ctry_dat_analysis$row.name = NULL
   
-  ctry_dat_analysis$potential_overlap_flag = ctry_dat_analysis$source_Organization_id %in% overlap_orgs
+  ctry_dat_analysis$potential_overlap_flag = ctry_dat_analysis$Recipient.Organization %in% overlap_orgs
+  ctry_dat_analysis$updatedAt = anydate(ctry_dat_analysis$updatedAt)
   fwrite(ctry_dat_analysis, paste0("output/",country,"_unformatted.csv"))
+  write.xlsx(ctry_dat_analysis, paste0("output/",country,"_unformatted.xlsx"))
   ctry_dat_analysis$originalAmountmn = ctry_dat_analysis$originalAmount/1000000
   ctry_dat_analysis$amountUSDmn = ctry_dat_analysis$amountUSD/1000000
   keep = c(
     "Country Code" = "destination_iso3",
     "Country" = "Destination.Country",
+    "Source" = "Donor",
     "Approval Date" = "analysis_date",
+    "date" = "date",
     "Last Updated" = "updatedAt",
     "Project Title" = "description",
     "Project ID" = "refCode",
@@ -101,14 +117,14 @@ for(i in 1:nrow(loop_param_df)){
   ctry_dat_analysis_formatted = ctry_dat_analysis[,keep,with=F]
   names(ctry_dat_analysis_formatted) = names(keep)
   ctry_dat_analysis_formatted = merge(ctry_dat_analysis_formatted,wb_groupings,by="Country Code",all.x=T)
-  ctry_dat_analysis_formatted[,c("Source","Classification","Loan Amount Committed (USD mn)","Comment","Press release/project website","Additional source")] = NA
+  ctry_dat_analysis_formatted[,c("Classification","Loan Amount Committed (USD mn)","Comment","Press release/project website","Additional source")] = NA
   ctry_dat_analysis_formatted$`Grant Portion` = 1
-  ctry_dat_analysis_formatted = ctry_dat_analysis_formatted[order(ctry_dat_analysis_formatted$`Approval Date`),]
+  ctry_dat_analysis_formatted = ctry_dat_analysis_formatted[order(ctry_dat_analysis_formatted$date),]
   ctry_dat_analysis_formatted$row.name = c(1:nrow(ctry_dat_analysis_formatted))
-  ctry_dat_analysis_formatted_l = ctry_dat_analysis_formatted[,c("row.name","Approval Date", "Total Disbursed")]
-  ctry_dat_analysis_formatted_l$variable = strftime(ctry_dat_analysis_formatted_l$`Approval Date`,format="%b-%y")
+  ctry_dat_analysis_formatted_l = ctry_dat_analysis_formatted[,c("row.name","date", "Total Disbursed")]
+  ctry_dat_analysis_formatted_l$variable = strftime(ctry_dat_analysis_formatted_l$date,format="%b-%y")
   ctry_dat_analysis_formatted_l$variable = factor(ctry_dat_analysis_formatted_l$variable, levels=unique(ctry_dat_analysis_formatted_l$variable))
-  ctry_dat_analysis_formatted_l$`Approval Date` = NULL
+  ctry_dat_analysis_formatted_l$date = NULL
   setnames(ctry_dat_analysis_formatted_l,"Total Disbursed","value")
   ctry_dat_analysis_formatted_w = dcast(ctry_dat_analysis_formatted_l,row.name~variable,sum)
   ctry_dat_analysis_formatted_w$row.name = NULL
@@ -140,5 +156,6 @@ for(i in 1:nrow(loop_param_df)){
   )
   ctry_dat_analysis_formatted = ctry_dat_analysis_formatted[,name_order,with=F]
   fwrite(ctry_dat_analysis_formatted, paste0("output/",country,"_formatted.csv"))
+  write.xlsx(ctry_dat_analysis_formatted, paste0("output/",country,"_formatted.xlsx"))
   rm(ctry_dat, ctry_dat_analysis, ctry_dat_remainder)
 }
